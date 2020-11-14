fad6d939d13c9324d2d04e8e0bfaf7b9379f01b3
maikeklemm <48826824+maikeklemm@users.noreply.github.com>
2020-11-14 22:45:06 +0100

    sdfg

--- L05_Potions/L05_potions_implementation/generateContent.ts
+++ L05_Potions/L05_potions_implementation/generateContent.ts
@@ -1,16 +1,16 @@
 namespace L04_Potions {
-    export interface Ingredient{
+    export interface Ingredient {
         name: string;
         price: number;
     }
- 
+
     export interface Data {
         [category: string]: Ingredient[];
     }
-    
 
 
-  export  function generateContent(_data:Data): void{
+
+    export function generateContent(_data: Data): void {
         console.log(_data);
 
         for (let category in _data) {
@@ -20,63 +20,63 @@ namespace L04_Potions {
             let group: HTMLElement | null = null;
             switch (category) {
 
-                           
+
                 case "Zutaten":
                     group = createIngredient(ingredient, category);
 
-            // let fieldset: HTMLFieldSetElement | null = document.querySelector("fieldset#Ingred");
-            // if (fieldset && group)
-            //     fieldset.insertBefore(group, document.getElementById("ok"));
+                    // let fieldset: HTMLFieldSetElement | null = document.querySelector("fieldset#Ingred");
+                    // if (fieldset && group)
+                    //     fieldset.insertBefore(group, document.getElementById("ok"));
                     break;
 
                 default:
                     break;
             }
 
-         
 
-        // if (items.length>0){
-        //     let group: HTMLDivElement = createIngredient(items,category)
 
-            
+            // if (items.length>0){
+            //     let group: HTMLDivElement = createIngredient(items,category)
+
+
             let fieldset: HTMLFieldSetElement | null = document.querySelector("fieldset#Ingred")
-            if(fieldset && group)
+            if (fieldset && group)
                 fieldset.appendChild(group)
 
-        
+
         }
     }
 
-        function createIngredient(_items: Ingredient[], _category: string): HTMLElement | null {
-            let group: HTMLDivElement = document.createElement("div");
-            group.id = "zutat";
-            for (let ingredient of _items) {
-                let checkbox: HTMLInputElement = document.createElement("input");
-                checkbox.type = "checkbox";
-                checkbox.setAttribute("price", ingredient.price.toFixed(2));
-                checkbox.value = ingredient.name;
-                checkbox.name = _category;
-                checkbox.id = ingredient.name;
-    
-                let label: HTMLLabelElement = document.createElement("label");
-                label.textContent = ingredient.name;
-                label.htmlFor = ingredient.name;
-    
-                group.appendChild(checkbox);
-                group.appendChild(label);   
+    function createIngredient(_items: Ingredient[], _category: string): HTMLElement | null {
+        let group: HTMLDivElement = document.createElement("div");
+        group.id = "zutat";
+        for (let ingredient of _items) {
+            let checkbox: HTMLInputElement = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.setAttribute("price", ingredient.price.toFixed(2));
+            checkbox.value = ingredient.name;
+            checkbox.name = _category;
+            checkbox.id = ingredient.name;
 
-        let Amount: HTMLInputElement = document.createElement("input");
-        Amount.type = "number";
-        Amount.value = "0";
-        Amount.name = ingredient.name + "Amount";
-        Amount.id = ingredient.name + "Amount";
-        Amount.min = "0";
-        Amount.setAttribute("amount", ingredient.price.toFixed(2));
-        group.appendChild(Amount);
+            let label: HTMLLabelElement = document.createElement("label");
+            label.textContent = ingredient.name;
+            label.htmlFor = ingredient.name;
 
-        let breakIt: HTMLElement = document.createElement("br");
-        group.appendChild(breakIt);
+            group.appendChild(checkbox);
+            group.appendChild(label);
+
+            let Amount: HTMLInputElement = document.createElement("input");
+            Amount.type = "number";
+            Amount.value = "0";
+            Amount.name = ingredient.name + "Amount";
+            Amount.id = ingredient.name + "Amount";
+            Amount.min = "0";
+            Amount.setAttribute("amount", ingredient.price.toFixed(2));
+            group.appendChild(Amount);
+
+            let breakIt: HTMLElement = document.createElement("br");
+            group.appendChild(breakIt);
+        }
+        return group;
     }
-    return group;
-}
 }
