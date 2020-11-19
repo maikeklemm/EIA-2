119bb12100b547e7af564cbfb3fd9e8289b37232
maikeklemm <48826824+maikeklemm@users.noreply.github.com>
2020-11-19 13:04:17 +0100

    dfghjm

--- L06_Potions/L06_potions_implementation/Server/Server.js
+++ L06_Potions/L06_potions_implementation/Server/Server.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.L04_Potions = void 0;
 const Http = require("http");
-const Url = require("url");
 var L04_Potions;
 (function (L04_Potions) {
     let server = Http.createServer();
@@ -15,17 +14,6 @@ var L04_Potions;
     server.addListener("request", handleRequest);
     function handleRequest() {
         console.log("Whats up?");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        if (_request.url) {
-            let url = Url.parse(_request.url, true);
-            for (let key in url.query) {
-                _response.write(key + ":" + url.query[key] + "<br/>");
-            }
-            let jsonString = JSON.stringify(url.query);
-            _response.write(jsonString);
-        }
-        _response.end();
     }
 })(L04_Potions = exports.L04_Potions || (exports.L04_Potions = {}));
 //# sourceMappingURL=Server.js.map
--- L06_Potions/L06_potions_implementation/Server/Server.ts
+++ L06_Potions/L06_potions_implementation/Server/Server.ts
@@ -17,19 +17,6 @@ export namespace L04_Potions {
     function handleRequest (): void{
         console.log("Whats up?");
 
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
 
-        if (_request.url) {
-            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-            for (let key in url.query) {
-                _response.write(key + ":" + url.query[key] + "<br/>");
-            }
-
-            let jsonString: string = JSON.stringify(url.query);
-            _response.write(jsonString);
-        }
-
-        _response.end();
     }
 }
